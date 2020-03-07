d3953c0c4589ff8c81985bd5339bf50263451b35
Abigail Ezedonmwen <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-07 12:26:49 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,18 +2,23 @@ let cityName = document.querySelector("#inputCityName").value;
 let stateName = document.querySelector("#inputStateName").value;
 let countryName = document.querySelector("#inputCountryName").value;
 let buttonElement = document.querySelector('button');
-JSON.stringify(cityName,stateName,countryName)
+const key = "4de906a7381ed6420744cd8f4f88c9f9";
+let= location = JSON.stringify("cityName"+"stateName"+"countryName")
 console.log(cityName)
 console.log(typeof cityName,typeof stateName,typeof countryName)
 
 
 
 buttonElement.addEventListener('click',(function(){
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=7c76bce7e335ad38fcf1dff70de1b0d6}`)
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateName},${countryName}&appid=${key}`)
     // .value parse calander input date only
       .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
       .then(response => {
-        console.log(response)
+        response.forEach((el.main.temp) => {
+        let p = document.createElement('p');
+        let location = document.querySelector('h2').innerHTML;
+
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
